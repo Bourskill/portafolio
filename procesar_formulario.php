0f6c4c30dfbc018dc008aae30ffd2f836f1941ad
Bourskill <criyeak@gmail.com>
2023-09-09 21:58:30 -0500

    master

---
+++ procesar_formulario.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST["nombre"];
+    $asunto = $_POST["asunto"];
+    $email = $_POST["email"];
+    $mensaje = $_POST["mensaje"];
+
+    $destinatario = "richard003800@gmail.com"; // Reemplaza con tu dirección de correo
+
+    $asunto_email = "Nuevo mensaje desde el formulario de contacto";
+
+    $mensaje_email = "Nombre: $nombre\n";
+    $mensaje_email .= "Asunto: $asunto\n";
+    $mensaje_email .= "Correo Electrónico: $email\n";
+    $mensaje_email .= "Mensaje:\n $mensaje";
+
+    mail($destinatario, $asunto_email, $mensaje_email);
+
+    header("Location: formulario.html");
+}
+?>
